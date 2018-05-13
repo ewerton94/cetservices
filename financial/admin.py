1330781f0539cfe2e4def8861c3ed9fe0053f684
Ewerton Amorim <ewerton.amorim@live.com>
2018-05-13 20:45:21 -0300

    Admin manager initial

--- financial/admin.py
+++ financial/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Entrance)
+admin.site.register(DebtInfo)
+admin.site.register(Debt)
+admin.site.register(Credit)
